751beaaa3d704218dd9fbcbbc317889c795368e1
Akshaydj <64835371+akshay8123-gadagoli@users.noreply.github.com>
2022-10-10 19:26:56 +0530

    Update binary_search.py

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,37 +1,28 @@
-# Python 3 program for recursive binary search.
+pos=-1
+
+def bsearch(list,n):
+    l=0
+    u=len(list)-1
+
+    while l <= u:
+        mid=(l+u) // 2 #initiating low high mid value
+        if list[mid]==n:
+         globals()['pos']==mid
+
+         return True
+        
 
 
-# Returns index of x in arr if present, else -1
-def binary_search(arr, low, high, x):
+        else:
+           if list[mid]<n:
+            l=mid+1 #changing mid value if element not found
+           else:
+            u=mid-1
 
-	if high >= low:
-
-		mid = (high + low) // 2
-
-		
-		if arr[mid] == x:
-			return mid
-
-		
-		elif arr[mid] > x:
-			return binary_search(arr, low, mid - 1, x)
-
-	
-		else:
-			return binary_search(arr, mid + 1, high, x)
-
-	else:
-		
-		return -1
-
-
-arr = [ 2, 3, 4, 10, 40 ]
-x = 10
-
-
-result = binary_search(arr, 0, len(arr)-1, x)
-
-if result != -1:
-	print("Element is present at index", str(result))
+    return False
+list=[45,65,78,85,90]
+n=(int(input('enter the no u want to search')))
+if bsearch(list, n):
+    print('found at',pos+1)
 else:
-	print("Element is not present in array")
+    print('not found')
